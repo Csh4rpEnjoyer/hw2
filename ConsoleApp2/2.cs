a264844c2c72e58e475f21cd5f4d459339e57637
Csh4rpEnjoyer <113981795+Csh4rpEnjoyer@users.noreply.github.com>
2022-09-25 21:02:46 +0300

    Delete ConsoleApp2 directory

--- ConsoleApp2/2.cs
+++
@@ -1,7 +0,0 @@
-﻿double x, y;
-x = double.Parse(Console.ReadLine());
-y = double.Parse(Console.ReadLine());
-if (Math.Abs(x + y) == 1)
-    Console.WriteLine("входит");
-else
-    Console.WriteLine("не входит");

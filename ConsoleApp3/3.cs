3addce9d86b379a07260e825ab92a2c3b1501135
Csh4rpEnjoyer <ricardomilosflex@mail.ru>
2022-09-25 21:02:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/3.cs
@@ -0,0 +1,8 @@
+﻿double x, y;
+x = double.Parse(Console.ReadLine());
+y = double.Parse(Console.ReadLine());
+
+if ((x >= -1 && x <= 1 && y <= x * x && y <= 1 && y <= 0) || ((x == 0) && (-1 <= y) && (y <= 0)))
+    Console.WriteLine("входит");
+else
+    Console.WriteLine("не входит");
